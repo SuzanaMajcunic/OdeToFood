84fe2d7dbe16d02e587d19f148233c8057919fa9
Suzana Majcunic <47116940+SuzanaMajcunic@users.noreply.github.com>
2019-09-03 22:07:55 +0200

    Restaurant and CousineType models added.

---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryResaurantData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryResaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        // SHORTCUT: ctor
+        public InMemoryResaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id=1, Name="Suzy's pizza", Cuisine = CuisineType.Italian },
+                new Restaurant { Id=2, Name="Destino", Cuisine = CuisineType.French },
+                new Restaurant { Id=1, Name="Rajesh finest food", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
